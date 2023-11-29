2e9a1379fbc126147ba762e3abb3eb1b066a387f
Jay Joo <jj534@cornell.edu>
2023-11-29 15:53:20 -0500

    fix fetch profile bug auto inserting undefined name column bug

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,6 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { LayoutLoad } from './$types';
-// import type { Database } from '../DatabaseDefinitions';
-import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutLoad = async ({ fetch, data, depends, url }) => {
 	depends('supabase:auth');
@@ -29,37 +27,19 @@ export const load: LayoutLoad = async ({ fetch, data, depends, url }) => {
 
 		if (profile) return profile;
 
-		// profile creation
-		switch (session?.user?.app_metadata?.provider) {
-			case 'email': {
-				if (url?.pathname !== '/setup-profile') {
-					throw redirect(307, `/setup-profile?${url.searchParams.toString()}`);
+		// automatic profile creation
+		const { data: newProfile } = await supabase
+			.from('profiles')
+			.insert([
+				{
+					id: session?.user?.id,
+					avatarUrl: session?.user?.user_metadata?.avatar_url
 				}
-				return null;
-			}
+			])
+			.select('*')
+			.single();
 
-			case 'google': {
-				const { data: profile } = await supabase
-					.from('profiles')
-					.insert([
-						{
-							id: session?.user?.id,
-							name: session?.user?.user_metadata?.full_name,
-							avatarUrl: session?.user?.user_metadata?.avatar_url
-						}
-					])
-					.select('*')
-					.single();
-
-				return profile;
-			}
-
-			// TODO: handle more providers
-
-			default: {
-				return null;
-			}
-		}
+		return newProfile;
 	};
 
 	return { supabase, session, profile: fetchProfile(session) };
